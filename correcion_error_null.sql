f7d8e0c5a03827c1c2236670da9daa66bc9ccc0b
mariferreras <marinaferreras@icloud.com>
2022-11-08 18:36:31 +0100

    mari8-11

--- correcion_error_null.sql
+++ correcion_error_null.sql
@@ -1,18 +1,16 @@
 USE project1;
 
 #Cambiamos los "ERROR" por NULL
-UPDATE data_sql
-SET q10_part_1 = NULL, 
-WHERE q10_part_1 = "ERROR"; 
 
 UPDATE data_sql
-SET q10_part_4 = NULL, q10_part_5 = NULL, q10_part_6 = NULL, q10_part_7 = NULL, 
+SET q10_part_1 = NULL, q10_part_2 = NULL, q10_part_3 = NULL,
+q10_part_4 = NULL, q10_part_5 = NULL, q10_part_6 = NULL, q10_part_7 = NULL, 
 q10_part_8 = NULL, q10_part_9 = NULL, q10_part_10 = NULL, q10_part_11 = NULL, 
 q10_part_12 = NULL, q10_part_13 = NULL, q10_part_14 = NULL, q10_part_15 = NULL, 
-q10_part_16 = NULL, 
-WHERE q10_part_4 = "ERROR" AND q10_part_5 = "ERROR" AND q10_part_6 = "ERROR" AND q10_part_7 = "ERROR" AND ; 
+q10_part_16 = NULL, q10_other = NULL, d482xta = NULL
 
-
-
-
-
+WHERE q10_part_1 = "ERROR" OR q10_part_2 = "ERROR" OR q10_part_3 = "ERROR" OR q10_part_4 = "ERROR" OR q10_part_5 = "ERROR" OR q10_part_6 = "ERROR" 
+OR q10_part_7 = "ERROR" OR q10_part_8 = "ERROR" OR q10_part_9 = "ERROR" 
+OR q10_part_10 = "ERROR" OR q10_part_11 = "ERROR" OR q10_part_12 = "ERROR" 
+OR q10_part_13 = "ERROR" OR q10_part_14 = "ERROR" OR q10_part_15 = "ERROR" OR q10_part_16 = "ERROR"
+OR q10_other = "ERROR" OR d482xta = "ERROR";
