b0e95578200f75c6cb6605b4c0b2bf030890592e
paulafuenteg <paula.fuenteg@outlook.es>
2022-11-08 15:00:30 +0100

    correcion_error_08/11

---
+++ correcion_error_null.sql
@@ -0,0 +1,18 @@
+USE project1;
+
+#Cambiamos los "ERROR" por NULL
+UPDATE data_sql
+SET q10_part_1 = NULL, 
+WHERE q10_part_1 = "ERROR"; 
+
+UPDATE data_sql
+SET q10_part_4 = NULL, q10_part_5 = NULL, q10_part_6 = NULL, q10_part_7 = NULL, 
+q10_part_8 = NULL, q10_part_9 = NULL, q10_part_10 = NULL, q10_part_11 = NULL, 
+q10_part_12 = NULL, q10_part_13 = NULL, q10_part_14 = NULL, q10_part_15 = NULL, 
+q10_part_16 = NULL, 
+WHERE q10_part_4 = "ERROR" AND q10_part_5 = "ERROR" AND q10_part_6 = "ERROR" AND q10_part_7 = "ERROR" AND ; 
+
+
+
+
+
